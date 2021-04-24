1fbf4c553a86d4807c1c5e4363c8a4fabe908999
Ionut-Alexandru Tudor <ionut95alex@gmail.com>
2021-04-25 02:46:39 +0300

    First Commit
    first commit with simple scene

---
+++ Assets/Scripts/Editor/GrpcHelper.cs
@@ -0,0 +1,59 @@
+﻿#if UNITY_EDITOR
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+
+using Debug = UnityEngine.Debug;
+
+public static class GrpcHelper
+{
+	[MenuItem("gRPC/Build proto files")]
+	private static void BuildProtoFiles()
+	{
+		Debug.Log("BuildProtoFiles");
+		try
+		{
+			var protoExe = Path.GetFullPath("Assets/Plugins/Grpc.Tools/protoc.exe");
+			var grpcPluginExe = Path.GetFullPath("Assets/Plugins/Grpc.Tools/grpc_csharp_plugin.exe");
+			var protoFileExtension = ".proto";
+
+			var protoFiles = Directory
+					.EnumerateFiles("Assets/Scripts/", "*.*", SearchOption.AllDirectories)
+					.Where(f => Path.GetExtension(f).ToLowerInvariant().Equals(protoFileExtension)).Select(p => Path.GetFullPath(p));
+
+			foreach (var protoFile in protoFiles)
+			{
+				var protoFileName = Path.GetFileName(protoFile);
+				Debug.Log($"Compiling protofile : {protoFileName}");
+
+				var directory = Path.GetDirectoryName(protoFile);
+
+				var arguments = $"/K {protoExe} -I {directory} --plugin=protoc-gen-grpc={grpcPluginExe} --csharp_out={directory} --grpc_out={directory} {protoFile}";
+
+				//Process.Start("cmd.exe", arguments);
+				
+				var processStartInfo = new ProcessStartInfo()
+				{
+					FileName = "cmd.exe",
+					UseShellExecute = false,
+					Arguments = arguments,
+					RedirectStandardOutput = true,
+					CreateNoWindow = false
+				};
+				var rezultProcess = Process.Start(processStartInfo);
+
+				var rezult = rezultProcess.StandardOutput.ReadToEnd();
+				rezultProcess.WaitForExit();
+			}
+		}
+		catch (Exception exception)
+		{
+			Debug.LogError(exception.Message);
+		}
+	}
+}
+
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # unity_grpc
+
+Steps A : https://intl.cloud.tencent.com/document/product/1055/39057\
+OR
+Steps B : 
+1. .NET 4.x
+	- in Unity3d, got to Edit > Project Setting > Player > Configuration > Scripting Runtime Version, change it to .Net 4.x
+2. grpc_unity_package
+	- go to https://packages.grpc.io/
+	- select the latest build Id
+	- select grpc_unity_package.VERSION.zip from the C# section
+	- extract the archive
+	- copy the Plugins folder in your unity Assets folder (Assets/Plugins)
